1007c738ac43fbb0724fab5972455337d9320696
saravanan-55-git <52695838+saravanan-55-git@users.noreply.github.com>
2022-10-03 23:33:30 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,60 @@
+let myResume={
+    "basics": {
+      "name": "SARAVANAN K",
+      "email": "vkssaran6@gamil.com",
+      "phone": 9080486749,
+      "degree": "M.C.A.,",
+      "location": {
+        "address": "no.625,madurai-trichy,mainroad,melur",
+        "postalCode": 625106,
+        "city": "Madurai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/saravanan-k-16777715/",
+          "github":"https://github.com/saravanan-55-git"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "XT Global",
+        "position": "software engineer",
+        "startDate": "26-sep-2022",
+        "endDate": "currently working",
+        "summary": "just started working..",
+      },
+    ],
+    "education": [
+      {
+        "institution": "raja college of engg & technology",
+        "department": "Computers",
+        "studyType": "fulltime",
+        "batch start year": 2001,
+        "batch end year": 2006,
+        "gpa": 7.0,
+      }
+    ],
+    "skills": [
+      {
+        "name": "java,javascript",
+        "level": "intermediate",
+        "project": [
+          "ETCC"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "playing outdoor games,basketball, shuttle badminton",
+      }
+    ]
+  }
+  console.log(myResume);
